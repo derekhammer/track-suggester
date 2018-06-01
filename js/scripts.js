e3da8771c6ec56fbea9ff4111bfca3fec8beadb2
Derek Hammer <derekhammer@hotmail.com>
2018-06-01 11:32:59 -0700

    it will correct spelling of parseInt and add values

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 $(document).ready(function(){
   $("#quiz").submit(function(event){
-    var team = parsInt($("input:radio[name=teamChoice]:checked").val());
-    var boss = parsInt($("input:radio[name=bossChoice]:checked").val());
-    var life = parsInt($("input:radio[name=lifeChoice]:checked").val());
-    var room = parsInt($("input:radio[name=roomChoice]:checked").val());
-    var city = parsInt($("input:radio[name=cityChoice]:checked").val());
+    debugger
+    var team = parseInt($("input:radio[name=teamChoice]:checked").val());
+    var boss = parseInt($("input:radio[name=bossChoice]:checked").val());
+    var life = parseInt($("input:radio[name=lifeChoice]:checked").val());
+    var room = parseInt($("input:radio[name=roomChoice]:checked").val());
+    var city = parseInt($("input:radio[name=cityChoice]:checked").val());
     var res = team + boss + life + room + city;
 
     if(res <=8) {
-      $("Show").show()
-    } else if (res <= 11){
-      $("Show").show()
+      $("#rrShow").show()
+    } else if (res <= 11) {
+      $("#cssShow").show()
     } else {
-      $("Show").show()
+      $("#csharpShow").show()
     }
 
     event.preventDefault();
